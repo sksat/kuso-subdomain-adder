4998f15540e17759cb1616e23b9c760b77ecfb45
sksat <sksat@sksat.net>
2020-12-27 07:40:08 +0900

    add: text record

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,68 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate cloudflare;
 extern crate punycode;
+extern crate toml;
+
+use std::fs;
+use std::io::Read;
+use std::sync::*;
 
 use actix_web::http::StatusCode;
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder, Result};
 
 use serde::{Deserialize, Serialize};
 
+use cloudflare::endpoints::dns;
+use cloudflare::framework::{
+    async_api,
+    async_api::ApiClient,
+    auth::Credentials,
+    response::{ApiFailure, ApiResponse, ApiResult},
+    Environment, HttpApiClientConfig,
+};
+
+#[derive(Deserialize)]
+struct Config {
+    token: String,
+    zone_identifier: String,
+}
+
+struct Data {
+    api_client: async_api::Client,
+    zone_identifier: String,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //println!("{}", punycode::encode("バーチャル六畳半").unwrap());
 
-    HttpServer::new(|| {
+    let mut cfg_file = fs::File::open("./config.toml")?;
+    let mut config = String::new();
+    cfg_file.read_to_string(&mut config)?;
+    let config: Config = toml::from_str(&config).unwrap();
+
+    let credentials = Credentials::UserAuthToken {
+        token: config.token,
+    };
+
+    let api_client = async_api::Client::new(
+        credentials,
+        HttpApiClientConfig::default(),
+        Environment::Production,
+    )
+    .unwrap();
+
+    let data = Data {
+        api_client,
+        zone_identifier: config.zone_identifier,
+    };
+    let data = Arc::new(Mutex::new(data));
+
+    HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
+            .data(data.clone())
             .configure(app_config)
     })
     .bind("127.0.0.1:8080")?
@@ -32,20 +83,75 @@ fn app_config(cfg: &mut web::ServiceConfig) {
     );
 }
 
-async fn index() -> Result<HttpResponse> {
+async fn index(data: web::Data<Arc<Mutex<Data>>>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; chaset=utf-8")
         .body(include_str!("../static/index.html")))
 }
 
-async fn handle_subdomain(params: web::Form<Subdomain>) -> Result<HttpResponse> {
+async fn handle_subdomain(
+    data: web::Data<Arc<Mutex<Data>>>,
+    params: web::Form<Subdomain>,
+) -> Result<HttpResponse> {
     let subdomain = if params.subdomain.chars().all(|c| c.is_ascii_alphanumeric()) {
         println!("{}", params.subdomain);
         params.subdomain.clone()
     } else {
         punycode::encode(&params.subdomain).unwrap()
     };
+
+    let params = dns::CreateDnsRecordParams {
+        name: &subdomain,
+        content: dns::DnsContent::TXT {
+            content: "hoge".to_string(),
+        },
+        priority: None,
+        proxied: None,
+        ttl: None,
+    };
+    create_records(&data.lock().unwrap(), params).await;
+
     Ok(HttpResponse::Ok()
         .content_type("text/plain")
         .body(subdomain))
+}
+
+async fn create_records(data: &Data, params: dns::CreateDnsRecordParams<'_>) {
+    let zone_identifier = &data.zone_identifier;
+    let cdr = dns::CreateDnsRecord {
+        zone_identifier,
+        params,
+    };
+    let response = data.api_client.request(&cdr).await;
+    match response {
+        Ok(success) => println!("success: {:?}", success),
+        Err(e) => match e {
+            ApiFailure::Error(status, errors) => {
+                println!("HTTP {}", status);
+            }
+            ApiFailure::Invalid(req_err) => println!("Error: {}", req_err),
+        },
+    }
+    //print_response(response);
+}
+
+fn print_response<T: ApiResult>(response: ApiResponse<T>) {
+    match response {
+        Ok(success) => println!("Success: {:#?}", success),
+        Err(e) => match e {
+            ApiFailure::Error(status, errors) => {
+                println!("HTTP {}:", status);
+                for err in errors.errors {
+                    println!("Error {}: {}", err.code, err.message);
+                    for (k, v) in err.other {
+                        println!("{}: {}", k, v);
+                    }
+                }
+                for (k, v) in errors.other {
+                    println!("{}: {}", k, v);
+                }
+            }
+            ApiFailure::Invalid(reqwest_err) => println!("Error: {}", reqwest_err),
+        },
+    }
 }
