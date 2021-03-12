b54fe41cf5b4d260d7ae997abed7b7fdf2f9370a
sksat <sksat@sksat.net>
2021-03-12 21:18:25 +0900

    add: tweet buttun

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::io::Read;
 use std::sync::*;
 
+use lazy_static::lazy_static;
+
 use actix_web::http::StatusCode;
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder, Result};
 
@@ -25,10 +27,29 @@ struct Config {
 struct Data {
     api_client: async_api::Client,
     zone_identifier: String,
+    context: tera::Context,
+}
+
+lazy_static! {
+    pub static ref TEMPLATES: tera::Tera = {
+        let mut tera = match tera::Tera::new("static/*") {
+            Ok(t) => t,
+            Err(e) => {
+                log::error!("template parse error: {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec!["html"]);
+        tera
+    };
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "kuso_subdomain_adder=info");
+    env_logger::init();
+
+    log::info!("kuso start");
     //println!("{}", punycode::encode("バーチャル六畳半").unwrap());
 
     let mut cfg_file = fs::File::open("./config.toml")?;
@@ -50,6 +71,7 @@ async fn main() -> std::io::Result<()> {
     let data = Data {
         api_client,
         zone_identifier: config.zone_identifier,
+        context: tera::Context::new(),
     };
     let data = Arc::new(Mutex::new(data));
 
@@ -79,9 +101,21 @@ fn app_config(cfg: &mut web::ServiceConfig) {
 }
 
 async fn index(data: web::Data<Arc<Mutex<Data>>>) -> Result<HttpResponse> {
+    let data = &mut data.lock().unwrap();
+    let context = &mut data.context;
+    context.insert("version", env!("CARGO_PKG_VERSION"));
+    let html = match TEMPLATES.render("index.html", &context) {
+        Ok(s) => s,
+        Err(e) => {
+            log::error!("render error: {}", e);
+            return Ok(HttpResponse::Ok()
+                .content_type("text/html")
+                .body("<script>alert('render error')</script>"));
+        }
+    };
     Ok(HttpResponse::Ok()
         .content_type("text/html; chaset=utf-8")
-        .body(include_str!("../static/index.html")))
+        .body(html))
 }
 
 async fn handle_subdomain(
@@ -89,32 +123,31 @@ async fn handle_subdomain(
     params: web::Form<Subdomain>,
 ) -> Result<HttpResponse> {
     let subdomain = if params.subdomain.chars().all(|c| c.is_ascii_alphanumeric()) {
-        println!("{}", params.subdomain);
+        log::info!("subdomain: {}", params.subdomain);
         params.subdomain.clone()
     } else {
         let pcode = punycode::encode(&params.subdomain).unwrap();
+        log::info!("subdomain: {} -> {}", &params.subdomain, &pcode);
         "xn--".to_string() + &pcode
     };
 
-    // add CNAME record
+    let content = "redirect.kuso.domains".to_string();
+    log::info!("add CNAME: {}", content);
     let record = dns::CreateDnsRecordParams {
         name: &subdomain,
-        content: dns::DnsContent::CNAME {
-            content: "redirect.kuso.domains".to_string(),
-        },
+        content: dns::DnsContent::CNAME { content },
         priority: None,
         proxied: None,
         ttl: None,
     };
     create_records(&data.lock().unwrap(), record).await;
 
-    // add TXT record
+    let content = params.url.clone();
+    log::info!("add TXT: {}", content);
     let txt_name = "_kuso-domains-to.".to_string() + &subdomain;
     let record = dns::CreateDnsRecordParams {
         name: &txt_name,
-        content: dns::DnsContent::TXT {
-            content: params.url.clone(),
-        },
+        content: dns::DnsContent::TXT { content },
         priority: None,
         proxied: None,
         ttl: None,
@@ -122,10 +155,35 @@ async fn handle_subdomain(
     create_records(&data.lock().unwrap(), record).await;
 
     // final URL
-    let url = "http://".to_string() + &subdomain + ".teleka.su";
-    let html = format!("<h2>URL: <a href=\"{}\">{}</a></h2>", url, url);
+    let protocol = "http://".to_string();
+    let domain = ".teleka.su";
+    let url = protocol.clone() + &subdomain + domain.clone();
+    let url_visual = protocol + &params.subdomain + domain;
+    log::info!("URL: {}", url);
 
-    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+    //let html = format!("<h2>URL: <a href=\"{}\">{}</a></h2>", url, url);
+    //Ok(HttpResponse::Ok().content_type("text/html").body(html))
+
+    let data = &mut data.lock().unwrap();
+    let context = &mut data.context;
+    context.insert("version", env!("CARGO_PKG_VERSION"));
+    context.insert("url", &url);
+    context.insert("url_visual", &url_visual);
+    context.insert("target_url", &params.url);
+    context.insert("share_text", &params.subdomain);
+
+    let html = match TEMPLATES.render("index.html", &context) {
+        Ok(s) => s,
+        Err(e) => {
+            log::error!("render error: {}", e);
+            return Ok(HttpResponse::Ok()
+                .content_type("text/html")
+                .body("<script>alert('render error')</script>"));
+        }
+    };
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; chaset=utf-8")
+        .body(html))
 }
 
 async fn create_records(data: &Data, params: dns::CreateDnsRecordParams<'_>) {
@@ -136,34 +194,33 @@ async fn create_records(data: &Data, params: dns::CreateDnsRecordParams<'_>) {
     };
     let response = data.api_client.request(&cdr).await;
     match response {
-        Ok(success) => println!("success: {:?}", success),
+        Ok(success) => log::info!("success: {:?}", success),
         Err(e) => match e {
-            ApiFailure::Error(status, errors) => {
-                println!("HTTP {}", status);
+            ApiFailure::Error(status, err) => {
+                log::error!("HTTP {}: {:?}", status, err);
             }
-            ApiFailure::Invalid(req_err) => println!("Error: {}", req_err),
+            ApiFailure::Invalid(req_err) => log::error!("Error: {}", req_err),
         },
     }
-    //print_response(response);
 }
 
 fn print_response<T: ApiResult>(response: ApiResponse<T>) {
     match response {
-        Ok(success) => println!("Success: {:#?}", success),
+        Ok(success) => log::info!("Success: {:#?}", success),
         Err(e) => match e {
             ApiFailure::Error(status, errors) => {
-                println!("HTTP {}:", status);
+                log::info!("HTTP {}:", status);
                 for err in errors.errors {
-                    println!("Error {}: {}", err.code, err.message);
+                    log::error!("Error {}: {}", err.code, err.message);
                     for (k, v) in err.other {
-                        println!("{}: {}", k, v);
+                        log::error!("{}: {}", k, v);
                     }
                 }
                 for (k, v) in errors.other {
-                    println!("{}: {}", k, v);
+                    log::error!("{}: {}", k, v);
                 }
             }
-            ApiFailure::Invalid(reqwest_err) => println!("Error: {}", reqwest_err),
+            ApiFailure::Invalid(reqwest_err) => log::error!("Error: {}", reqwest_err),
         },
     }
 }
