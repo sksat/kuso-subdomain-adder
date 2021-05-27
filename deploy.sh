afc81d00050bc9afd10f6232adfd5f0e267fbb0b
sksat <sksat@sksat.net>
2021-05-27 10:09:30 +0900

    add: update check for repo & container image

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,68 @@
 #!/bin/bash
 cd $(dirname $0)
 
+if [ ! -f .env ]; then
+	echo ".env does not exist."
+	exit 1
+fi
+
+source .env
+
+function get_remote_img(){
+	local tag=$1
+	local user=sksat
+	local repo=kuso-subdomain-adder
+
+	echo -n "${tag} "
+	curl --silent \
+		--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+		--header "Authorization: Bearer" \
+		"https://ghcr.io/v2/${user}/${repo}/manifests/${tag}" \
+		| jq -r '.config.digest'
+}
+
+function get_local_img(){
+	local tag=$1
+	docker images --no-trunc --digests ghcr.io/sksat/kuso-subdomain-adder --format '{{.Tag}} {{.ID}}' | grep $tag
+}
+
+function update_repo(){
+	local branch=`git symbolic-ref --short HEAD`
+	local local_commit=`git rev-parse HEAD`
+	local remote_commit=`git ls-remote origin ${branch} | awk '{print $1}'`
+
+	echo "local:  ${local_commit}"
+	echo "remote: ${remote_commit}"
+
+	if [[ $local_commit != $remote_commit ]]; then
+		echo "pulling repository..."
+		git pull origin $branch
+	fi
+	echo ""
+}
+
+function update_image(){
+	echo "update docker image..."
+
+	local local_img=$(get_local_img $VERSION)
+	local remote_img=$(get_remote_img $VERSION )
+
+	echo "local:  $local_img"
+	echo "remote: $remote_img"
+
+	if [[ $local_img != $remote_img ]]; then
+		echo "pulling new images..."
+		docker-compose pull
+	fi
+	echo ""
+}
+
 function update(){
 	local is_running=`docker-compose ps --services --filter "status=running"`
 
-	echo "pulling repository..."
-	git pull
-	echo "pulling new images..."
-	docker-compose pull
+	update_repo
+	update_image
+
 	if [[ "$is_running" != "" ]]; then
 		echo "services are still running!"
 		docker-compose down
