f7ad09e9e129a69b5385e71f6aee4711a25190e4
sksat <sksat@sksat.net>
2021-10-19 07:25:00 +0900

    fix: vergen API is kasu

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
-use vergen::{vergen, Config};
+use vergen::{vergen, Config, TimestampKind};
 
 fn main() -> Result<()> {
     let mut cfg = Config::default();
     *cfg.git_mut().sha_kind_mut() = vergen::ShaKind::Short;
+    *cfg.build_mut().kind_mut() = TimestampKind::All;
     vergen(cfg)
 }
